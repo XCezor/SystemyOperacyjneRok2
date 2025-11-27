4196c5f28629a87d8f3fb53092cb229e02a8a1c2
Szymon Grabarczyk <xcezor@gmail.com>
2025-11-27 13:29:38 +0100

    save

--- OS/Zad8/compr-bench.sh
+++ OS/Zad8/compr-bench.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-create_temp_folder() {
-    tmp_dir=$(mktemp -d)
-}
-
-if [[ $# -lt 2 ]]; then
+if [[ $# -lt 1 ]]; then
     echo "Script requires at least one folder to compress."
-    return 0
-else do
-    create_temp_folder
+    exit 1
+else
+    tmp_dir=$(mktemp -d)
 
     for arg in "$@"; do
         tar cvf "$tmp_dir/archive.tar" $arg
+        echo "$tmp_dir/archive.tar" | gzip -c > plik.gz
     done
 fi
